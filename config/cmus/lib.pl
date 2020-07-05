6bfcefe418d23551750fa98b1914e3ca54d1424e
MrExcaliburBr <joseeduardo3224@gmail.com>
2020-07-05 17:17:43 -0300

    configs

---
+++ config/cmus/lib.pl
@@ -0,0 +1,221 @@
+/home/zezin/music/'Chocolate Rain' Original Song by Tay Zonday.mp3
+/home/zezin/music/1Kilo - Deixe Me Ir (MANNELL & João Oliveira Remix).mp3
+/home/zezin/music/1Kilo - Duro Igual Concreto (Luccas Remix).mp3
+/home/zezin/music/Absofacto - Dissolve.mp3
+/home/zezin/music/Alien Boy.mp3
+/home/zezin/music/All That.mp3
+/home/zezin/music/Android52 - Super Anime Groove.mp3
+/home/zezin/music/Apashe - Lacrimosa (Figure x Dont Kill It Remix).mp3
+/home/zezin/music/Aquarela do Brasil.mp3
+/home/zezin/music/BIG SHAQ - MANS NOT HOT (Sad Meal Dubstep Remix).mp3
+/home/zezin/music/Beatness - Nightcall (Kavinsky Cover).mp3
+/home/zezin/music/Beethoven - Für Elise (Klutch Dubstep Trap Remix).mp3
+/home/zezin/music/Believe In Music.mp3
+/home/zezin/music/Birthday (feat. Jeff Kaale).mp3
+/home/zezin/music/Bonetrousle.mp3
+/home/zezin/music/Borgore - Decisions.mp3
+/home/zezin/music/Brasil Cuíqueiro - Lofi Hip Hop.mp3
+/home/zezin/music/Breakbot - Baby I'm Yours feat. Irfane.mp3
+/home/zezin/music/Buttercup.mp3
+/home/zezin/music/CYBRPNK & Sullivan King - MSHPIT.mp3
+/home/zezin/music/Caleb Belkin - Water.mp3
+/home/zezin/music/Calvin Harris - Feel So Close.mp3
+/home/zezin/music/Cash Machine.mp3
+/home/zezin/music/Chico Buarque - Mambembe.mp3
+/home/zezin/music/Childish Gambino - Redbone (Official Audio)-Kp7eSUU9oy8.mp3
+/home/zezin/music/Crônicas de Steve.mp3
+/home/zezin/music/Cuco - Bossa No Sé ft. Jean Carte.mp3
+/home/zezin/music/Cuco - Keeping Tabs (feat. Suscat0).mp3
+/home/zezin/music/Daryl Hall & John Oates - You Make My Dreams.mp3
+/home/zezin/music/Dirt Monkey - Lift Off.mp3
+/home/zezin/music/Dirtyphonics & Sullivan King - Vantablack.mp3
+/home/zezin/music/DragonForce - Through The Fire And Flames.mp3
+/home/zezin/music/EARFQUAKE.mp3
+/home/zezin/music/Englewood - Crystal Dolphin.mp3
+/home/zezin/music/Evilwave & Antima - Tinnitus.mp3
+/home/zezin/music/FEDDIE DREDD - WHEATHER.mp3
+/home/zezin/music/FREDDIE DREDD - GO! (PROD. RYAN C).mp3
+/home/zezin/music/FREDDIE DREDD - GTG (PROD. RYAN C).mp3
+/home/zezin/music/FREDDIE DREDD - REDRUM (PROD. RYAN C).mp3
+/home/zezin/music/Freddie Dredd - Cha Cha.mp3
+/home/zezin/music/Freddie Dredd - Opaul.mp3
+/home/zezin/music/Future - Mask Off.mp3
+/home/zezin/music/GILBERTO GIL EXTRA.mp3
+/home/zezin/music/Goosebumps (Theme Song).mp3
+/home/zezin/music/Gorillaz - 19-2000 .mp3_new.mp3
+/home/zezin/music/Gorillaz - Andromeda .mp3
+/home/zezin/music/Gorillaz - Aries ft. Peter Hook & Georgia.mp3
+/home/zezin/music/Gorillaz - Broken - Plastic Beach.mp3
+/home/zezin/music/Gorillaz - Clint Eastwood.mp3_new.mp3
+/home/zezin/music/Gorillaz - DARE .mp3_new.mp3
+/home/zezin/music/Gorillaz - Desole.mp3
+/home/zezin/music/Gorillaz - Dirty Harry.mp3_new.mp3
+/home/zezin/music/Gorillaz - Doncamatic.mp3
+/home/zezin/music/Gorillaz - El Manana .mp3
+/home/zezin/music/Gorillaz - Feel Good Inc..mp3
+/home/zezin/music/Gorillaz - Hollywood feat. Snoop Dogg & Jamie Principle.mp3
+/home/zezin/music/Gorillaz - Humility .mp3
+/home/zezin/music/Gorillaz - On Melancholy Hill.mp3_new.mp3
+/home/zezin/music/Gorillaz - Rock The House .mp3
+/home/zezin/music/Gorillaz - Saturnz Barz (Spirit House).mp3
+/home/zezin/music/Gorillaz – Friday 13th ft. Octavian.mp3
+/home/zezin/music/Green Greens - Kirby Super Star-QCQa2tIXWok.mp3
+/home/zezin/music/HOME - Resonance (Last Island Remake).mp3
+/home/zezin/music/HOME - Resonance.mp3
+/home/zezin/music/HOME - Tides.mp3
+/home/zezin/music/HOME - We're Finally Landing.mp3
+/home/zezin/music/Hippie Sabotage - Devil Eyes.mp3
+/home/zezin/music/Home - Living Forever As You Are Now.mp3
+/home/zezin/music/Hovey Benjamin - Send Bobs.mp3
+/home/zezin/music/Hurt.mp3
+/home/zezin/music/I Love U, Pt. 1.mp3
+/home/zezin/music/I Love U, Pt. 2.mp3
+/home/zezin/music/Jack Stauber - Doctor(feat. Sarah).mp3
+/home/zezin/music/JoJo Part 4 - Diamond is Unbreakable - Opening 3 Full『Great Days』by Karen Aoki.mp3
+/home/zezin/music/JoJo Part 5 - Golden Wind OST - Torture Dance Song Full『Canzoni Preferite』.mp3
+/home/zezin/music/JoJo's Bizarre Adventure (DUBSTEP Remix).mp3
+/home/zezin/music/JoJo's Bizarre Adventure OP1 Sono Chi no Sadame.mp3
+/home/zezin/music/JoJo's Bizarre Adventure Opening 5 Full 'CRAZY NOISY BIZARRE TOWN'.mp3
+/home/zezin/music/JoJo's Bizarre Adventure opening 6 Full『Chase - batta』.mp3
+/home/zezin/music/Jojo's Bizarre Adventure OP 2  UHD 2160p  Creditless.mp3
+/home/zezin/music/Kero Kero Bonito - Flamingo.mp3
+/home/zezin/music/Kill The Noise & Snails - Shake The Ground (feat. Sullivan King & Jonah Kay).mp3
+/home/zezin/music/Knife Party - 'Bonfire'.mp3
+/home/zezin/music/LAKEY INSPIRED - Better Days.mp3
+/home/zezin/music/Ligia - Tom Jobim (Lofi remix).mp3
+/home/zezin/music/MACROSS 82-99 - Fun Tonight.mp3
+/home/zezin/music/Marcos Valle - Estrelar.mp3
+/home/zezin/music/Memorex Memories - It's Very Sunny.mp3
+/home/zezin/music/Memorex Memories - Pick Me Up (feat. Ultraviolet Pilot).mp3
+/home/zezin/music/Memorex Memories - Pictures of Purple Skies.mp3
+/home/zezin/music/Memorex Memories - Thanks for Listening.mp3
+/home/zezin/music/Memorex Memories - Wasting Time.mp3
+/home/zezin/music/Men At Work - Down Under.mp3
+/home/zezin/music/Men At Work - Overkill.mp3
+/home/zezin/music/Men At Work - Who Can It Be Now.mp3
+/home/zezin/music/Mine All Day (Minecraft Music Video).mp3
+/home/zezin/music/Miracle Man.mp3
+/home/zezin/music/Moe Shop - You Look So Good [Pure Pure EP].mp3
+/home/zezin/music/Momentary Bliss (feat. slowthai and Slaves).mp3
+/home/zezin/music/Naruto Shippuden opening 2 - You Are My Friend.mp3
+/home/zezin/music/Nintendo Wee.mp3
+/home/zezin/music/Nintendo膣69 - ブラジル蒸気モリ.mp3
+/home/zezin/music/Novo Amor - From Gold (HOME Remix).mp3
+/home/zezin/music/Nujabes - Aruarian dance.mp3
+/home/zezin/music/Nujabes - Counting Stars.mp3
+/home/zezin/music/Nujabes - Feather.mp3
+/home/zezin/music/Nujabes - Lady Brown.mp3
+/home/zezin/music/Nujabes - Love Sick (part 2).mp3
+/home/zezin/music/Nujabes - Ordinary Joe.mp3
+/home/zezin/music/Nujabes - The Final View.mp3
+/home/zezin/music/Nujabes - Voice of Autumn.mp3
+/home/zezin/music/Ocean Man.mp3
+/home/zezin/music/Oliver Tree - Alien Boy (Big Data Remix).mp3
+/home/zezin/music/Oliver Tree - All I Got [Official Audio].mp3
+/home/zezin/music/Oliver Tree - Hurt (NGHTMRE Remix).mp3
+/home/zezin/music/PPAP Pen Pineapple Apple Pen (BOIRIA Remix).mp3
+/home/zezin/music/Panda Eyes - Colorblind.mp3
+/home/zezin/music/Panthurr - paradise.mp3
+/home/zezin/music/Pegboard Nerds x Misterwives - Coffins [EDM].mp3
+/home/zezin/music/Persona 5 - Last Surprise OFFICIAL Lyrics.mp3
+/home/zezin/music/RIOT - Overkill.mp3
+/home/zezin/music/Rae Sremmurd - Black Beatles ft. Gucci Mane.mp3
+/home/zezin/music/Ramones - Poison Heart.mp3
+/home/zezin/music/Redbone - Come and get Your Love.mp3
+/home/zezin/music/Remmi - Star Spangled (Not Your Dope x Taptone Remix).mp3
+/home/zezin/music/Rex Orange County - Best Friend (Official Audio)-OqBuXQLR4Y8.mp3
+/home/zezin/music/Rex Orange County - Loving is Easy (feat. Benny Sings).mp3
+/home/zezin/music/Roddy Ricch - The Box [Official Audio].mp3
+/home/zezin/music/Rolling Down The Street, In My Katamari.mp3
+/home/zezin/music/Rooftop Run (Day) - Sonic Unleashed.mp3
+/home/zezin/music/Rooftop Run (Modern) - Sonic Generations.mp3
+/home/zezin/music/Roundabout (2008 Remastered Version).mp3
+/home/zezin/music/SKRILLEX - KYOTO (FT. SIRAH).mp3
+/home/zezin/music/SKRILLEX - Rock N' Roll (Will Take You To The Mountain).mp3
+/home/zezin/music/Seu Jorge-Mina do Condominio.mp3
+/home/zezin/music/Shaggy - Mr. Bombastic.mp3
+/home/zezin/music/Shing02 + Nujabes - Luv(sic.) part 1.mp3
+/home/zezin/music/Shirtsleeves - Garota de Ipanema (Tom Jobim Cover) _ Lo-fi.mp3
+/home/zezin/music/Skrillex & Damian 'Jr. Gong' Marley - Make It Bun Dem [OFFICIAL VIDEO].mp3
+/home/zezin/music/Skrillex & Diplo - 'Mind' feat. Kai (Official Video).mp3
+/home/zezin/music/Skrillex & The Doors - Breakn' A Sweat.mp3
+/home/zezin/music/Skrillex - Bangarang (Ft. Sirah) [Official Audio].mp3
+/home/zezin/music/Skrillex - First Of The Year (Equinox) [Official Audio].mp3
+/home/zezin/music/Skrillex - Kill Everybody (Official Audio).mp3
+/home/zezin/music/Skrillex - Scary Monsters And Nice Sprites (Official Audio).mp3
+/home/zezin/music/Snail's House - Pixel Galaxy.mp3
+/home/zezin/music/Snail's House × Moe Shop - Pastel.mp3
+/home/zezin/music/Snoop Dogg & Wiz Khalifa - Young, Wild and Free ft. Bruno Mars.mp3
+/home/zezin/music/Sonic 1 Music - Labyrinth Zone.mp3
+/home/zezin/music/Sonic 2 Music - Chemical Plant Zone.mp3
+/home/zezin/music/Sonic Adventure 2 'City Escape'.mp3
+/home/zezin/music/Soulful (Original Mix).mp3
+/home/zezin/music/Spider Dance.mp3
+/home/zezin/music/Super Mario World Game Over LoFi Hip Hop Remix.mp3
+/home/zezin/music/Superfast Jellyfish - a visual accompaniment.mp3
+/home/zezin/music/TIM MAIA - GOSTAVA TANTO DE VOCE.mp3
+/home/zezin/music/Teminite - Beastmode.mp3
+/home/zezin/music/The Jackson 5 - I Want You Back.mp3
+/home/zezin/music/The Misfits Angelfuck.mp3
+/home/zezin/music/The Pelones   Porkys Mother3 Extended.mp3
+/home/zezin/music/They Tryna Be Cray ft. Young Nudy.mp3
+/home/zezin/music/Tim Maia - Que Beleza.mp3
+/home/zezin/music/Tim Maia - Réu Confesso.mp3
+/home/zezin/music/Tim Maia - Sossego.mp3
+/home/zezin/music/Travis Scott - NO BYSTANDERS.mp3
+/home/zezin/music/Travis Scott - goosebumps ft. Kendrick Lamar.mp3
+/home/zezin/music/Tribalistas - Já Sei Namorar (Mock X Eric Mello).mp3
+/home/zezin/music/Tribalistas - Já Sei Namorar.mp3
+/home/zezin/music/Undertale OST - Bergentrückung + ASGORE.mp3
+/home/zezin/music/VANTAGE - 50_50.mp3
+/home/zezin/music/Vanilla - Fuji.mp3
+/home/zezin/music/Wait a Minute! - Willow Smith.mp3
+/home/zezin/music/Walk Like an Egyptian.mp3
+/home/zezin/music/We Are Number One (MadRats Dubstep Remix).mp3
+/home/zezin/music/We can dance if we want to.mp3
+/home/zezin/music/When It Rains.mp3
+/home/zezin/music/Whethan VS Oliver Tree - When I'm Down.mp3
+/home/zezin/music/Who Came After - Internet Legion.mp3
+/home/zezin/music/Yung NutQuick - Cheat Codes for Hoes.mp3
+/home/zezin/music/ZarastrutA - 'Lá Fora'.mp3
+/home/zezin/music/cantando eu mando a tristeza embora.mp3
+/home/zezin/music/driver - the eye of truth.mp3
+/home/zezin/music/i wish it would never stop snowing .mp3
+/home/zezin/music/jinsang - feelings..mp3
+/home/zezin/music/peachy - falling for u (feat. mxmtoon).mp3
+/home/zezin/music/potsu - just friends.mp3
+/home/zezin/music/potsu - lugares.mp3
+/home/zezin/music/saib. - Brazil.mp3
+/home/zezin/music/saib. - Ipanema.mp3
+/home/zezin/music/saib. - Sunflowers.mp3
+/home/zezin/music/saib. - São Paulo Sunset.mp3
+/home/zezin/music/siddartha. -  beautiful love.mp3
+/home/zezin/music/yung lixo - assalto ao trap ft. bife (prod bife).mp3
+/home/zezin/music/yung lixo - brilhar (official audio).mp3
+/home/zezin/music/yung lixo - coca zero (prod bife _ ef _ pmm) (official audio).mp3
+/home/zezin/music/yung lixo - desgraça (prod bife).mp3
+/home/zezin/music/yung lixo - drip veneno.mp3
+/home/zezin/music/yung lixo - phones3x ft. yumaru (official audio) [prod EF e BIFE].mp3
+/home/zezin/music/yung lixo - porsche panamera [prod bife].mp3
+/home/zezin/music/yung lixo - sahara [prod. bife].mp3
+/home/zezin/music/yung lixo - snow [prod bife].mp3
+/home/zezin/music/yung lixo - vans preto (official audio) [prod. bife].mp3
+/home/zezin/music/【Chill Trap】WARR!OR - The Time Is Now ft. Dey Jesselin.mp3
+/home/zezin/music/【Dubstep】Ablaze x Korptic - Cosmic Inflaction.mp3
+/home/zezin/music/【Dubstep】Au5 & Heavy J - Dream Of Love feat. Kenny Raye (Venemy Remix).mp3
+/home/zezin/music/【Dubstep】Knife Party & Tom Morello - Battle Sirens (RIOT Remix).mp3
+/home/zezin/music/【Future Bass】ATB - Ecstasy (ARMNHMR & DATHAN Flip).mp3
+/home/zezin/music/【Future Bass】Adventure Club Ft ELEA - Dreams (NIGHTOWLS X Fransis Derelle Remix).mp3
+/home/zezin/music/【Future Bass】AmpEra ft. Repel the Robot - Last Call [Exclusive].mp3
+/home/zezin/music/【Future Bass】Lostboycrow - Powers (Kill Paris Remix).mp3
+/home/zezin/music/【Future Bass】Remmi - Star Spangled (Not Your Dope x Taptone Remix).mp3
+/home/zezin/music/【Future Bass】Tarro - Wave Board.mp3
+/home/zezin/music/【Future Bass】Tarro x Chris Poirier - Pixel Voices.mp3
+/home/zezin/music/【Future Bass】ronaldoreagan x Shmittee - Ethereal.mp3
+/home/zezin/music/【Future Trap】ronaldoreagan x Shmittee - Ethereal.mp3
+/home/zezin/music/【Trap】Aero Chord - The 90s.mp3
+/home/zezin/music/【Trap】Fransis Derelle x Sightlow - OG.mp3
+/home/zezin/music/Ｊｏａｏ　Ｇｉｌｂｅｒｔｏ　Ｖｉｂｅｓ.mp3
+/home/zezin/music/ＴＩＭ　ＭＡＩＡ  －  ＧＯＳＴＡＶＡ　ＴＡＮＴＯ　ＤＥ　ＶＯＣＥ  ［ｌｏｆｉ　ｒｅｍｉｘ］.mp3
+/home/zezin/music/ＴＩＭ　ＭＡＩＡ - ＥＬＡ　ＰＡＲＴＩＵ (Lofi remix).mp3
+/home/zezin/music/ｆａｌａｄｏｒ.mp3
